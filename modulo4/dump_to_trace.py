206a9571b483b8038e1877f16819c8868b7de09f
Murilo Matsubara <murilo.matsubara@gmail.com>
2021-10-02 21:24:03 -0300

    Correções no script python para fazer gerar trace com acesso de memória correto

--- modulo4/dump_to_trace.py
+++ modulo4/dump_to_trace.py
@@ -7,16 +7,21 @@ def main():
         return
 
     input_file = sys.argv[1]
-    output_file = sys.argv[2]     
+    output_file = sys.argv[2]        
 
     with open(input_file) as input, open(output_file, "w") as output:    
         pattern = re.compile(r"(   [\dabcdef]*:)|((ld|lw|lwu|lh|lhu|lb|lbu|fld|flw|sd|sw|sh|sb|fsd|fsw)[^#\n]*)")
         result = pattern.findall(input.read())
+
+        test = result[-1]
+        offset = int(result[0][0].strip(":   "), base=16)
+        sp = int(result[-1][0].strip(":   "), base=16) - offset
+
         for line, inst, _ in result:
             if (line):
-                output.write(to_trace(line) + "\n")
+                output.write(to_trace(line, sp, offset) + "\n")
             if (inst):
-                trace = to_trace(inst)
+                trace = to_trace(inst, sp, offset)
                 if (trace != ""):
                     output.write(trace + "\n")
 
@@ -29,10 +34,10 @@ def check_command():
         return False
     return True
 
-def to_trace(instruction):
+def to_trace(instruction, sp, offset):
     # Instruction fetch:
-    if (instruction.endswith(":")):
-        return "2 {}".format(instruction.strip(":   "))
+    if (instruction.endswith(":") and instruction.startswith("   ")):
+        return "2 {}".format(hex(int(instruction.strip(":   "), base=16) - offset).split("0x")[1])
 
     label = 0 if instruction[0] == "l" or instruction[0] == "f" or instruction[:1] == "fs" else 1
 
@@ -41,7 +46,7 @@ def to_trace(instruction):
         addr = re.search(",-?\d*", instruction).group(0).strip(",")
         if addr == "":
             return ""
-        return "{} {}".format(label, to_abs_hex(addr))
+        return "{} {}".format(label, to_abs_hex(int(addr, base=16) + sp))
 
     return ""
 
